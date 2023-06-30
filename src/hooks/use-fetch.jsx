b0349b0b143d7e242fcca320f97e8e41dd9375dc
bm9avan <mggpavan@gmail.com>
2023-06-30 20:04:24 +0530

    added .env file

--- src/hooks/use-fetch.jsx
+++ src/hooks/use-fetch.jsx
@@ -6,7 +6,7 @@ const useFetch = () => {
     const callFetch = useCallback(
         async (obj, dataHandler) => {
             setLoading(true)
-            const responce = await fetch('https://expences-traker-default-rtdb.asia-southeast1.firebasedatabase.app/expences.json', obj)
+            const responce = await fetch(`${process.env.REACT_APP_API_URL}/expences.json`, obj)
             const data = await responce.json()
             dataHandler(data)
             setLoading(false)
